528eaecbb5bdaf722c71e7042604d4474388adee
Korepanov Vyacheslav <real93@live.ru>
2016-01-02 13:22:23 +0400

    Zsh configuration files added.
    zsh/conf.d/aliases.zsh contains aliases to ls and grep.
    zsh/conf.d/bindings.zsh contains bindings of delete, home, end keys.
        In input mode up and down keys are using to history search.
    zsh/conf.d/options.zsh contains different options. More information in man zshoptions.
    zsh/conf.d/prompt_vars.zsh contains bindings of prompt symbols to environment variables.
    zsh/zlogin can be used as ~/.zlogin. Currently it runs a 'screenfetch' command.
    zsh/zshrc can be used as ~/.zshrc. It enables completion, gentoo specific completion.
        Also it run 'startx' command if user login on /dev/tty6 and it run tmux by default.

---
+++ zsh/conf.d/aliases.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+#
+# aliases.zsh
+# Copyright (C) 2016 real <real93@live.ru>
+#
+# Distributed under terms of the MIT license.
+#
+
+# ls output with colors, directories first, with indicators
+alias ls='ls --color=auto --group-directories-first --classify'
+alias grep='grep --color=auto'
